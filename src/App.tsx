cf66f3640bb0f1a08237803a4d6415b6cf5ef95c
Antonio Papa <antoniopapa1991@gmail.com>
2020-12-13 10:52:42 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Main from "./pages/Main";
+import {BrowserRouter, Route} from "react-router-dom";
+import Login from "./public/Login";
+import Register from "./public/Register";
+import Rankings from "./pages/Rankings";
+import Stats from "./pages/Stats";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Route exact path={'/'} component={Main}/>
+                <Route path={'/login'} component={Login}/>
+                <Route path={'/register'} component={Register}/>
+                <Route path={'/rankings'} component={Rankings}/>
+                <Route path={'/stats'} component={Stats}/>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+import {Provider} from 'react-redux';
+import configureStore from "./redux/configureStore";
+import constants from './constants';
+
+axios.defaults.baseURL = constants.BASE_URL;
+axios.defaults.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
